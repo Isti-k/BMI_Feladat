c6a56b61892878f025ce03fc74e00476a4fc957a
Isti-k <kiscserkes03@gmail.com>
2024-03-21 12:04:43 +0100

    jav

--- src/bmifeladat/BmiFeladat.java
+++ src/bmifeladat/BmiFeladat.java
@@ -4,17 +4,47 @@
  */
 package bmifeladat;
 
+import java.util.Scanner;
+
 
 public class BmiFeladat {
 
-    
+
+
+
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         bmiFeladat();
         
     }
 
-    private static void bmiFeladat() {
-        
+    static void bmiFeladat() {
+        kiir(kerdes);
+        beker();
+        int m = sc.nextInt("");
+        int t = sc.nextInt("");
+        ertekeles();
     }
+    
+    private static void ertekeles() {
+        String ki = "Ön %d cm magas és %d kg tömegű".formatted(magassag, tomeg);
+        String alkat = "";
+        if(bmi < 20){
+            
+        }
+    }
+
+
+    private static int beker(String kerdes, int also, int felso) {
+        kiir(kerdes);
+        int adat;
+        do{
+            adat = sc.nextInt();
+        } while (adat < also || adat > felso);
+        
+
+        return adat;
+    }
+
     
 }
